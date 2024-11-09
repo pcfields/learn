97d94e67ae61e5fcb799816f470b8b91d74f5601
Peter <pcfields@gmail.com>
2024-11-09 17:37:14 +0100

    add first test

--- react-apps/pomodoro/src/timer/timer.test.tsx
+++ react-apps/pomodoro/src/timer/timer.test.tsx
@@ -1,8 +1,11 @@
 import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
 import { Timer } from "./timer";
 
 describe("Timer", () => {
   test("should display hours", () => {
-    expect(<Timer />).toBeInTheDocument();
+    render(<Timer />);
+
+    expect(screen.getByText("timer")).toBeInTheDocument();
   });
 });
